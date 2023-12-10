5a399abbbf4c7118603fd9748f13926139877034
Ben Kabongo <kabongo.ben025@gmail.com>
2023-12-10 18:41:54 +0100

    Seq2Seq Model

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -22,22 +22,25 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
 
-    def __init__(self, input_size, hidden_size, num_layers=1):
+    def __init__(self, n_dim, input_size, hidden_size, projection_size, num_layers=1):
         super().__init__()
         self.rnn = nn.GRU(input_size, hidden_size, num_layers=num_layers, batch_first=True)
+        self.project_fc = nn.Linear(hidden_size, projection_size)
+        self.out_fc = nn.Linear(projection_size, n_dim)
+        self.relu = nn.ReLU()
         
-
     def forward(self, input, h0):
         output, hn = self.rnn(input, h0)
+        output = self.out_fc(self.relu(self.project_fc(output)))
         return output, hn
 
 
 class Seq2SeqModel(nn.Module):
 
-    def __init__(self, encoder, decoder, output_length) -> None:
+    def __init__(self, n_dim, output_length, input_size, hidden_size, projection_size, num_layers):
         super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
+        self.encoder = Encoder(input_size, hidden_size, num_layers)
+        self.decoder = Decoder(n_dim, input_size, hidden_size, projection_size, num_layers)
         self.output_length = output_length
 
     def forward(self, X):
